58d949db5d90c705215967b081a9e6ae139e6112
Dan Arki <danarki6@gmail.com>
2023-06-01 11:44:47 +0200

    changes for azat

--- Downloads/greener-bientot-finis-master/src/pages/AskCoalBill.jsx
+++ Downloads/greener-bientot-finis-master/src/pages/AskCoalBill.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import useCheckMobileScreen from '../util/useCheckMobileScreen';
-import NumberInput from '../components/NumberInput';
-import { Arrow } from './AskPropertyType';
-import Navbar from '../components/Navbar';
-import { generateHeatEnergyURLstring } from '../util/generateURLstring';
-import MobileLayout from '../layouts/MobileLayout';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useCheckMobileScreen from "../util/useCheckMobileScreen";
+import NumberInput from "../components/NumberInput";
+import { Arrow } from "./AskPropertyType";
+import Navbar from "../components/Navbar";
+import { generateHeatEnergyURLstring } from "../util/generateURLstring";
+import MobileLayout from "../layouts/MobileLayout";
 
 const AskCoalBill = () => {
   const isMobile = useCheckMobileScreen();
-  const [coalBill, setCoalBill] = React.useState('');
+  const [coalBill, setCoalBill] = React.useState("");
   const navigate = useNavigate();
   const url = generateHeatEnergyURLstring();
 
@@ -19,12 +19,12 @@ const AskCoalBill = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('coalBill', coalBill);
+    localStorage.setItem("coalBill", coalBill);
     navigate(url);
   };
   const IDKHandler = () => {
-    setCoalBill('NILL');
-    localStorage.setItem('coalBill', 'NILL');
+    setCoalBill("NILL");
+    localStorage.setItem("coalBill", "NILL");
     navigate(url);
   };
 
@@ -43,7 +43,7 @@ const AskCoalBill = () => {
             changeHandler={changeHandler}
             IDKHandler={IDKHandler}
             nextNav="/heatenergy"
-            label={'Montant par mois'}
+            label={"Montant par mois"}
             placeholder="Exemple: 60 €"
             unit="€/mois"
           />
@@ -55,45 +55,45 @@ const AskCoalBill = () => {
     <>
       <div className="flex flex-col h-screen ">
         <Navbar one two />
-         <div className="flex h-full">
+        <div className="flex h-full">
           {/* LEFT */}
           <div className="w-[30%] bg-[#FCFFFE] h-full flex items-center justify-center shadow-2xl">
             <img src="/assets/home3d.png" alt="" />
           </div>
-        <div className="flex-1 flex flex-col gap-4 w-full px-16 pt-6 pb-4 ">
-          <button
-            onClick={() => navigate(-1)}
-            className="flex items-center gap-3"
-          >
-            <Arrow />
-            <p className="text-xl text-[#1E1D4C] font-medium ">Retour</p>
-          </button>
-          <div className="w-full flex items-center justify-start h-4 mb-4 bg-[#FCFFFE] rounded-full">
-            <div
-              className="h-3 bg-[#8DD9DE] rounded-full m-1 shadow-bar"
-              style={{ width: '20%' }}
-            ></div>
+          <div className="flex-1 flex flex-col gap-4 w-full px-16 pt-6 pb-4 ">
+            <button
+              onClick={() => navigate(-1)}
+              className="flex items-center gap-3"
+            >
+              <Arrow />
+              <p className="text-xl text-[#1E1D4C] font-medium ">Retour</p>
+            </button>
+            <div className="w-full flex items-center justify-start h-4 mb-4 bg-[#FCFFFE] rounded-full">
+              <div
+                className="h-3 bg-[#8DD9DE] rounded-full m-1 shadow-bar"
+                style={{ width: "20%" }}
+              ></div>
+            </div>
+
+            <section className="flex-1 flex flex-col items-center">
+              <h1 className="font-semibold text-2xl text-[#1E1D4C] py-6">
+                Quelle est le montant de votre facture de charbon ?
+              </h1>
+
+              <NumberInput
+                isMobile={isMobile}
+                state={coalBill}
+                changeHandler={changeHandler}
+                onSubmit={onSubmit}
+                IDKHandler={IDKHandler}
+                nextNav="/goals"
+                label={"Montant par mois"}
+                placeholder="Exemple: 60 €"
+                unit="€/mois"
+              />
+            </section>
           </div>
-
-          <section className="flex-1 flex flex-col items-center">
-            <h1 className="font-semibold text-2xl text-[#1E1D4C] py-6">
-              Quelle est le montant de votre facture de charbon ?
-            </h1>
-
-            <NumberInput
-              isMobile={isMobile}
-              state={coalBill}
-              changeHandler={changeHandler}
-              onSubmit={onSubmit}
-              IDKHandler={IDKHandler}
-              nextNav="/goals"
-              label={'Montant par mois'}
-              placeholder="Exemple: 60 €"
-              unit="€/mois"
-            />
-          </section>
         </div>
-          </div>
       </div>
     </>
   );
